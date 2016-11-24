c85719add138ce7795fa40aa1ce31e315705622f
gabrielstuff <gabriel@soixantecircuits.fr>
2016-11-24 23:56:40 +0100

    🍏💛🍎💙add chalk package for colors in the console output

--- actions/mailchimp.js
+++ actions/mailchimp.js
@@ -1,7 +1,19 @@
 'use strict'
 
 const Mailchimp = require('mailchimp-api-v3')
+const chalk = require('chalk')
 const config = require('../config/config.json')
 
 var mailchimp = new Mailchimp(config.actioner.mailchimp.api_key)
+
+mailchimp.post('/lists/id', {
+  email_address : '...'
+  
+})
+.then(function(results) {
+  
+})
+.catch(function (err) {
+  console.error()
+})
 
