e5754bbbb6e9d9651b027c6384d4e794135560be
gabrielstuff <gabriel@soixantecircuits.fr>
2016-11-25 00:53:29 +0100

    📝add server.port option

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const fs = require('fs-extra')
 const router = express.Router()
 const app = express()
 
+const version = 'v1'
+
 app.use(morgan('dev'))
-app.use('/api/v1', router)
+app.use(`/api/${version}`, router)
 
 app.get('/', (req, res) => { res.send('See https://github.com/soixantecircuits/altruist for details.') })
 
@@ -27,4 +29,7 @@ for (action in config.actions) {
   })
 }
 
-app.listen(6060)
+app.listen(config.server.port, function(){
+  console.log(`altruist runs on: http://localhost:${config.server.port}`)
+  console.log(`Point your browser at: http://localhost:${config.server.port}/api/${version}/actions/{action} to test`)
+})
