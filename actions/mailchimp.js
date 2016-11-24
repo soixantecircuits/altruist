d7f8a727a32f70d9cf0f991e6a8d7f1d9ce9242d
gabrielstuff <gabriel@soixantecircuits.fr>
2016-11-25 00:27:31 +0100

    📨 🐒add mailchimp subscription

--- actions/mailchimp.js
+++ actions/mailchimp.js
@@ -2,26 +2,43 @@
 
 const Mailchimp = require('mailchimp-api-v3')
 const chalk = require('chalk')
+const _ = require('lodash')
+
 const config = require('../config/config.json')
-const fakeData = require('../mock/fakdedata.json')
+const fakeData = require('../mock/fakedata.json')
 
 var mailchimp = new Mailchimp(config.actioner.mailchimp.apiKey)
 
 var email = fakeData.receiver.email
 var lname = fakeData.receiver.lname
 var fname = fakeData.receiver.fname
-
+console.log(config.actioner.mailchimp.listID)
 mailchimp.post(`/lists/${config.actioner.mailchimp.listID}`, {
-  email_address : email,
-  merge_fields:{
-    LNAME: lname,
-    FNAME: fname
-  }
+  members: [
+    {
+      email_address: email,
+      merge_fields: {
+        LNAME: lname,
+        FNAME: fname
+      },
+      status: 'subscribed'
+    }
+  ]
 })
-.then(function(results) {
-  console.log(chalk.blue(results))
+.then(function (results) {
+  if(results.errors !== undefined && results.errors.length > 0){
+    console.error(chalk.red('Error'))
+    _.each(results.errors, function(error){
+      console.error(chalk.red(`${error.email_address}: ${error.error}`))
+    })  
+  } else {
+    console.log(chalk.blue('Success:'))
+    if(results.new_members){
+      console.log(results.new_members[0])
+    }
+  }
 })
 .catch(function (err) {
   console.error(chalk.red(err))
+  console.error(err.errors)
 })
-
