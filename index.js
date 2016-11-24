7f258f2fb690ac9f51b7e3d584a1b9ac277563c9
hugohil <hugo@soixantecircuits.fr>
2016-11-25 00:00:19 +0100

    🚗 Add auto action routing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const fs = require('fs-extra')
 
 const router = express.Router()
 const app = express()
+
+let actions = [ 'mailchimp' ]
 
 app.use(morgan('dev'))
 app.use('/api/v1', router)
@@ -10,5 +13,19 @@ app.use('/api/v1', router)
 app.get('/', (req, res) => { res.send('See https://github.com/soixantecircuits/altruist for details.') })
 
 router.get('/status', (req, res) => { res.send('up') })
+
+actions.forEach((action) => {
+  const module = `${__dirname}/actions/${action}.js`
+  router.get(`/actions/${action}`, (req, res) => {
+    fs.access(module, (err) => {
+      if (err) {
+        res.status(403).send('no such action')
+      } else {
+        require(module).init()
+        res.send('exists')
+      }
+    })
+  })
+})
 
 app.listen(6060)
